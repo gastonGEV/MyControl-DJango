e52d06af2c478c3d7f10ce54824bcd80b96c9b54
gastonGEV <38236659+gastonGEV@users.noreply.github.com>
2018-10-30 21:38:37 -0300

    update

--- myControl/urls.py
+++ myControl/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     #path('', views.post_list, name='post_list'),
     path('', views.incidencia_list, name='incidencia_list'),
+    path('<int:id>', views.incidencia_list, name='incidencia_list'),
     path('show/<int:id>', views.incidencia, name='incidencia'),
     path('calendar/<int:id>', views.calendar, name='calendar'),
 ]
--- myControl/views.py
+++ myControl/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Incidencia
 from django.db.models import Sum
+from pprint import pprint
+from django.core.paginator import Paginator
 
 # def post_list(request):
 #     posts = Post.objects.filter(
 #         published_date__lte=timezone.now()).order_by('published_date')
 #     return render(request, 'myControl/post_list.html', {'posts': posts})
 
-def incidencia_list(request):
-    incidencias = Incidencia.objects.all()
-    ingresos = Incidencia.objects.filter(tipo=1).aggregate(Sum('mount'))
-    gastos = Incidencia.objects.filter(tipo=2).aggregate(Sum('mount'))
+def incidencia_list(request, id=1):
+    incidencia_list = Incidencia.objects.filter(created_date__month=id)
+    ingresos = Incidencia.objects.filter(tipo=1, created_date__month=id).aggregate(Sum('mount'))
+    gastos = Incidencia.objects.filter(tipo=2, created_date__month=id).aggregate(Sum('mount'))
+
+    paginator = Paginator(incidencia_list, 5)  # Show 10 incidencias per page
+
+    page = request.GET.get('page')
+    incidencias = paginator.get_page(page)
+
     return render(request, 'myControl/incidencias.html', {'incidencias': incidencias, 'ingresos':ingresos, 'gastos':gastos})
 
 def incidencia(request, id):
@@ -19,5 +27,7 @@ def incidencia(request, id):
     return render(request, 'myControl/show.html', {'incidencia': incidencia})
 
 def calendar(request, id):
+    mes = Incidencia.objects.filter(created_date__month=id)[:1]
     incidencias = Incidencia.objects.filter(created_date__month=id)
-    return render(request, 'myControl/calendar.html', {'incidencias':incidencias})
+    
+    return render(request, 'myControl/calendar.html', {'incidencias':incidencias, 'mes':mes})
